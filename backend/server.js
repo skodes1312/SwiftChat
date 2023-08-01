611ea062ef09a934d6863994b62f77191fe198ca
skodes1312 <kannaujiyasunny1312@gmail.com>
2023-08-01 16:23:07 +0530

    Setup backend

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import { chats } from "./data/data.js";
+import cors from "cors";
+
+//configuring dotenv file
+
+dotenv.config();
+const app = express();
+
+//setting cors middleware to allow cross-origin resource sharing
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("Working Fine.");
+});
+
+app.get("/api/chats", (req, res) => {
+  res.send(chats);
+});
+
+const PORT = process.env.PORT || 8000;
+
+//starting server
+
+app.listen(PORT, () => {
+  console.log(`Server running at Port:${PORT}`);
+});
